e73748474a9054d5173d20d757c12cb681061bbe
Joe Lv <joel@wicresoft.com>
2018-06-25 15:01:12 +0800

    first commit

---
+++ App70/App70.Android/SplashActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App70.Droid
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/MainTheme", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.splash_layout);
+            // Create your application here
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Toast.MakeText(this, "1111", ToastLength.Short).Show();
+            Handler handler = new Handler();
+            handler.PostDelayed(() => {
+                StartActivity(typeof(MainActivity));
+                OverridePendingTransition(0, 0);
+            }, 3000);
+            
+        }
+    }
+}
